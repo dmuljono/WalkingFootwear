1fe695eb1dcfebb11d03220b5e5ad628e0ebea0f
ash141999 <107966430+ash141999@users.noreply.github.com>
2022-08-14 14:42:56 -0500

    Added find product by name

--- Current Iteration/SecurityTestImpl/src/main/java/jor/empapp/repositorys/ProductRepository.java
+++ Current Iteration/SecurityTestImpl/src/main/java/jor/empapp/repositorys/ProductRepository.java
@@ -2,12 +2,16 @@ package jor.empapp.repositorys;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jor.empapp.models.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//	public List<Product> findByCategoryId(Long id);
+	public List<Product> findByCategoryCategoryId(Long id);
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
